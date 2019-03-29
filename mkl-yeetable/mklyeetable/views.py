c8a66b5a5d18a7789dada4685bacd78a177fb557
Mercedez Castro <mercedez@hawaii.edu>
2019-03-28 18:24:23 -1000

    Update

--- mkl-yeetable/mklyeetable/views.py
+++ mkl-yeetable/mklyeetable/views.py
@@ -1,16 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from yeelight import Bulb
+from yeelight import discover_bulbs
 
 def about(request):
     return render(request, 'about.html')
 
 def index(request):
-    # return HttpResponse('index')
+    bulb = Bulb("10.3.3.144")
     if request.GET.get('connect'):
         print(request.GET.get('connect'))
-        print("Button pressed")
-        bulb = Bulb("10.3.3.127")
+        print("Discovering")
+        print(discover_bulbs())
+    elif request.GET.get('turnon'):
+        print(request.GET.get('turnon'))
+        print("Turning On")
         bulb.turn_on()
+    elif request.GET.get('turnoff'):
+        print(request.GET.get('turnoff'))
+        print("Turning Off")
+        bulb.turn_off()
+    elif request.GET.get('dimming'):
+        print(request.GET.get('dimming'))
+        print("Dimming")
+        bulb.set_brightness(1)
+    elif request.GET.get('color'):
+        print(request.GET.get('color'))
+        print("Setting Color")
+        bulb.set_color_temp(2000)
     return render(request, 'index.html')
-
--- mkl-yeetable/users/views.py
+++ mkl-yeetable/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from yeelight import Bulb
+from yeelight import discover_bulbs
 
 # Create your views here.
 def signup_view(request):
@@ -14,6 +16,27 @@ def signup_view(request):
     return render(request, 'users/signup.html', { 'form': form })
 
 def user_homepage(request):
+    bulb = Bulb("10.3.3.144")
+    if request.GET.get('connect'):
+        print(request.GET.get('connect'))
+        print("Discovering")
+        print(discover_bulbs())
+    elif request.GET.get('turnon'):
+        print(request.GET.get('turnon'))
+        print("Turning On")
+        bulb.turn_on()
+    elif request.GET.get('turnoff'):
+        print(request.GET.get('turnoff'))
+        print("Turning Off")
+        bulb.turn_off()
+    elif request.GET.get('dimming'):
+        print(request.GET.get('dimming'))
+        print("Dimming")
+        bulb.set_brightness(1)
+    elif request.GET.get('color'):
+        print(request.GET.get('color'))
+        print("Setting Color")
+        bulb.set_color_temp(2000)
     return render(request, 'users/userhome.html')
 
 def login_view(request):
